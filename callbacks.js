0fa78894e1fd74d27b4ad0d21681004c24d4cbb9
Ian Gallardo <gallard3@gmail.com>
2024-07-19 20:35:47 -0400

    challenge 1 to 6

--- callbacks.js
+++ callbacks.js
@@ -2,34 +2,71 @@
 console.log("Hello, world!");
 
 // Challenge 1
-function addTwo(num) {}
+function addTwo(num) {
+  return num + 2;
+}
 
 // To check if you've completed it, uncomment these console.logs!
 // console.log(addTwo(3));
 // console.log(addTwo(10));
 
 // Challenge 2
-function addS(word) {}
+function addS(word) {
+  return word + "s";
+}
 
 // uncomment these to check your work
 // console.log(addS('pizza'));
 // console.log(addS('bagel'));
 
 // Challenge 3
-function map(array, callback) {}
+function map(array, callback) {
+  //return array.map((array) => callback(array))
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callback(array[i]));
+  }
+  return newArray;
+}
 
 // console.log(map([1, 2, 3], addTwo));
 
 // Challenge 4
-function forEach(array, callback) {}
+function forEach(array, callback) {
+  array.forEach((element) => callback(element));
+}
 
 // see for yourself if your forEach works!
 
+/*
+let alphabet = '';
+const letters = ['a', 'b', 'c', 'd'];
+forEach(letters, function(char) {
+  alphabet += char;
+});
+console.log(alphabet);
+*/
+
 // Challenge 5
-function mapWith(array, callback) {}
+function mapWith(array, callback) {
+  let newArray = [];
+  array.forEach((element) => newArray.push(callback(element)));
+  return newArray;
+}
 
 // Challenge 6
-function reduce(array, callback, initialValue) {}
+function reduce(array, callback, initialValue) {
+  let accumulator = initialValue;
+
+  for (let i = 0; i < array.length; i++) {
+    accumulator = callback(accumulator, array[i]);
+  }
+  return accumulator;
+}
+
+// const nums = [4, 1, 3];
+// const add = function(a, b) { return a + b; }
+// console.log(reduce(nums, add, 0));   //-> 8
 
 // Challenge 7
 function intersection(arrays) {}
